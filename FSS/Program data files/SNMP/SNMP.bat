f52d5f9612a2f6ae0858e7a1f0aa72d635f2111a
HojunUSF <hojunlee@usf.edu>
2022-11-29 19:29:59 -0500

    Fix 11/29

--- FSS/Program data files/SNMP/SNMP.bat
+++ FSS/Program data files/SNMP/SNMP.bat
@@ -20,20 +20,14 @@ echo START IN 1 SECOND
 timeout /t 1 /nobreak >nul
 cls
 
-set /p out= Yes or No
+set /p out= Y or N
 echo.
-if %out%==yes goto dm
-if %out%==Yes goto dm
 if %out%==y goto dm
-if %out%==Y goto dm
-if %out%==No goto dh
-if %out%==no goto dh
-if %out%==N goto dh
 if %out%==n goto dh
 
 
 :dm
-start PowerSNMP.vbs
+start SNMPtool.bat
 timeout 0
 start SNMPsystem.bat
 timeout 0
@@ -43,8 +37,6 @@ FS1.exe
 
 :dh
 start Snmpcontrol.bat
-timeout 0
-start alert.vbs
 timeout 0
 start SNMPsystem.bat
 timeout 0
